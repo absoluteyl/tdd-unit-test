56a590ae9798b639c8efa6aa277f42a234143947
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2024-01-06 21:01:07 +0800

    test: refactor set_budgets to take multiple budget input
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- spec/budget_service_spec.rb
+++ spec/budget_service_spec.rb
@@ -12,35 +12,34 @@ RSpec.describe BudgetService, "#query" do
 
   context "when query whole month" do
     it "should return 310" do
-      set_budgets
+      set_budgets([{yearMonth: '202401', amount: 310}])
       expect(query_budget('2024010101', '20240131')).to eq 310
     end
   end
 
   context "when query single date of a month" do
     it "should return 10" do
-      set_budgets
+      set_budgets([{yearMonth: '202401', amount: 310}])
       expect(query_budget('2024010101', '2024010101')).to eq 10
     end
   end
 
   context "when query cross months" do
     it "should return " do
-      budget_repo = double
-      allow(budget_repo).to receive(:getAll).and_return([
-        Budget.new('202401', 310),
-        Budget.new('202402', 2900)
+      set_budgets([
+        {yearMonth: '202401', amount: 310},
+        {yearMonth: '202402', amount: 2900}
       ])
       expect(BudgetService.new(budget_repo).query('20240130', '20240205')).to eq 520
     end
   end
 
-  def set_budgets
-    @budget_repo = double
-      allow(@budget_repo).to receive(:getAll).and_return([
-        Budget.new('202401', 310)
-      ])
-  end
+def set_budgets(budgets)
+  @budget_repo = double
+  allow(@budget_repo).to receive(:getAll).and_return(budgets.map{ |b|
+    Budget.new(b[:yearMonth], b[:amount])
+  })
+end
 
   def query_budget(start_date, end_date)
     BudgetService.new(@budget_repo).query(start_date, end_date)
