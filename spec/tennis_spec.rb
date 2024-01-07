ad4dceffe14017e936721e8b47af6f2d54ba06dd
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2024-01-07 21:09:04 +0800

    refactor: to ruby taste method calling syntax

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -3,8 +3,8 @@ require 'tennis'
 RSpec.describe Tennis, "#score" do
   context "when game start" do
     it "should return love all" do
-      tennis = Tennis.new()
-      expect(tennis.score()).to eq("Love All")
+      tennis = Tennis.new
+      expect(tennis.score).to eq("Love All")
     end
   end
 end
